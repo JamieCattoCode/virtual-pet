9c033b0f48ada2c26e27dbb00d34f5ae0486b5b5
Jamie Catto <jamietcatto@gmail.com>
2022-10-17 10:39:34 +0200

    Added growUp method with test.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -7,7 +7,21 @@ describe('constructor', () => {
 
     it('sets name property in constructor', () => {
         const fides = new Pet('Fido');
-
         expect(fides.name).toEqual('Fido');
+    })
+
+    it('sets initial age as 0', () => {
+        const fides = new Pet('Fido');
+        expect(fides.age).toStrictEqual(0);
+    })
+})
+
+describe('growUp method', () => {
+    it('growUp method increments age by 1', () => {
+        const fides = new Pet('Fido');
+        fides.growUp();
+        expect(fides.age).toStrictEqual(1);
+        fides.growUp();
+        expect(fides.age).toStrictEqual(2);
     })
 })
